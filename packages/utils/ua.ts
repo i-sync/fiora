2d232ea8556194043388592ea83c330d71eb249c
root <root@LAPTOP-7KFRJUTS>
2025-01-18 22:36:21 +0800

    update ua add view=mobile.

--- packages/utils/ua.ts
+++ packages/utils/ua.ts
@@ -4,4 +4,8 @@ export const isiOS = /iPhone/i.test(UA);
 
 export const isAndroid = /android/i.test(UA);
 
-export const isMobile = isiOS || isAndroid;
+// 添加URL参数检测
+export const forceMobile = new URLSearchParams(window.location.search).get('view') === 'mobile';
+
+// 修改isMobile判断，增加forceMobile条件
+export const isMobile = isiOS || isAndroid || forceMobile;
